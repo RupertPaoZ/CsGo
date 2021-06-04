ef079f5191e3eb6bf99b028038c74006f3a4bbf1
rupertpaoz <1589669635@qq.com>
2021-06-04 21:39:22 +0800

    整理

--- src/BinGen.sh
+++
@@ -1,6 +0,0 @@
-#filename=$(basename "$1" .ll)
-# filename="../test/course_assist/test"
-filename="../test/course_assist/c_assist2"
-echo $filename
-llc -filetype=obj $filename.ll  -o $filename.o
-clang $filename.o -o $filename.out
